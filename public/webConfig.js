8a4605b03fbbd9237a7cfea21ceea678d3a2b676
James Preston <jamescpreston93@gmail.com>
2018-05-12 20:28:22 +0100

    Add a web config to alter your requests and add form to HTML

---
+++ public/webConfig.js
@@ -0,0 +1,8 @@
+var webConfig = {
+   "url": "http://localhost:3000",
+   "verb": "POST",
+   "params": [
+      { "name": "userId", "value": "1" },
+      { "name": "ammount", "value": "1000" }
+   ]
+};
