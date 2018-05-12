bec95abb7164712f182281b2a55420f470623630
James Preston <jamescpreston93@gmail.com>
2018-05-12 19:45:05 +0100

    Add html and css file for form

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res, next) {
+   res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(process.env.PORT || 3000);
