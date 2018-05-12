5a62dd5f03c6b08e7e591fcec5d1af24545f27ec
James Preston <jamescpreston93@gmail.com>
2018-05-12 20:50:30 +0100

    Add readme and update config file

--- public/webConfig.js
+++ public/webConfig.js
@@ -1,8 +1,8 @@
 var webConfig = {
-   "url": "http://localhost:3000",
+   "url": "http://localhost:3000/transfer",
    "verb": "POST",
    "params": [
       { "name": "userId", "value": "1" },
-      { "name": "ammount", "value": "1000" }
+      { "name": "amount", "value": "1000" }
    ]
 };
